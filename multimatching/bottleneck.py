aed652c446b6e18a7bd42d58d44404259c111ddd
donsheehy <don.r.sheehy@gmail.com>
2022-02-03 15:35:35 -0500

    getting the code to work.

--- multimatching/bottleneck.py
+++ multimatching/bottleneck.py
@@ -5,39 +5,39 @@ from pdsketch import Diagram
 
 
 def dB(A: Diagram, B: Diagram, upperbound=float('inf')) -> float:
-    list_A = A.get_point_mass_lists()[0]
-    list_B = B.get_point_mass_lists()[0]
-    n_A = sum(1 for a in list_A)
-    n_B = sum(1 for a in list_B)
-    n = len(list_A)
-    diag_A = len(list_A)
-    diag_B = len(list_A) + len(list_B) + 1
+    # list_A = A.get_point_mass_lists()[0]
+    # list_B = B.get_point_mass_lists()[0]
+    n_A = sum(A.mass.values())
+    n_B = sum(B.mass.values())
+    n = len(A) + 1
+    diag_A = len(A)
+    diag_B = len(A) + len(B) + 1
     source = diag_B + 1
     sink = source + 1
     expected_flow = n_A + n_B
 
     edges = []
     Edge = namedtuple('Edge', ['distance', 'start', 'end', 'capacity'])
-    for i, a in enumerate(list_A):
+    for i, (a, m) in enumerate(A.mass.items()):
         # add edge from source to points in A
-        edges.append(Edge(0, source, i, 1))
-        # add edge from
-        edges.append(Edge(a.pp_dist(a.diagproj()), i, diag_B, 1))
+        edges.append(Edge(0, source, i, m))
+        # add edge from each point to the diagonal
+        edges.append(Edge(a.pp_dist(a.diagproj()), i, diag_B, m))
     # Next, add the edges from B to the sink.
-    for j, b in enumerate(B, start=n):
-        edges.append(Edge(0, j, sink, 1))
-        edges.append(Edge(b.pp_dist(b.diagproj()), diag_A, j, 1))
+    for j, (b, m) in enumerate(B.mass.items(), start=n):
+        # add edges to from points to the sink
+        edges.append(Edge(0, j, sink, m))
+        # add edges from diagonal to the points
+        edges.append(Edge(b.pp_dist(b.diagproj()), diag_A, j, m))
     # Add edges with the diagonal.
     edges.append(Edge(0, diag_A, diag_B, min(n_A, n_B)))
     edges.append(Edge(0, source, diag_A, n_B))
     edges.append(Edge(0, diag_B, sink, n_A))
     # Only include the edges from A to B that might be matched.
-    for i, a in enumerate(A):
-        for j, b in enumerate(B, start=n):
-            if a.pp_dist(b) <= min(upperbound, max(a.pp_dist(a.diagproj()), b.pp_dist(b.diagproj(
-
-            )))):
-                edges.append(Edge(a.dist(b), i, j, 1))
+    for i, (a, a_mass) in enumerate(A.mass.items()):
+        for j, (b, b_mass) in enumerate(B.mass.items(), start=n):
+            if a.pp_dist(b) <= min(upperbound, max(a.l_inf_dist(a.diagproj()), b.l_inf_dist(b.diagproj()))):
+                edges.append(Edge(a.dist(b), i, j, min(a_mass, b_mass)))
     # Sort the eges by length
     edges.sort()
 
--- multimatching/test_bottleneck.py
+++ multimatching/test_bottleneck.py
@@ -1,14 +1,41 @@
 from pdsketch import Diagram
-
+import unittest
 from multimatching import dB
 
-
-# import pytest
-
-class TestNoMult:
+class TestNoMult(unittest.TestCase):
     def test_no_mult(self):
         A = Diagram([(3, 6), (1, 2), (6, 18), (1, 3)])
         B = Diagram([(4, 6), (7, 21)])
 
         bdist = dB(A, B)
-        assert bdist == 3.0
+        self.assertEqual(bdist, 3.0)
+
+    def test_one_point_with_mult(self):
+        #  Two diagrams with multiplicity
+        A = Diagram([(0,6)],[1000000000])
+        B = Diagram([(1,6)],[1000000000])
+        self.assertEqual(dB(A,B), 1.0)
+
+    def test_one_point_with_different_mults(self):
+        #  Two diagrams with multiplicity
+        A = Diagram([(0,6)],[1000000001])
+        B = Diagram([(1,6)],[1000000000])
+        self.assertEqual(dB(A,B), 3.0)
+
+    def test_multiple_points_with_different_mults(self):
+        #  Two diagrams with multiplicity with ponts close to the diagonal
+        A = Diagram([(0,6), (0,1)],[1000000001, 100])
+        B = Diagram([(100, 101), (1,6)],[50, 1000000000])
+        self.assertEqual(dB(A,B), 3.0)
+
+    def test_unequal_size_base_sets(self):
+        #  Two diagrams with multiplicity with ponts close to the diagonal
+        A = Diagram([(0,6), (0,1)],[1000000001, 100])
+        B = Diagram([(1,6), (100, 101), (0,8)],[1000000000, 50, 1])
+        self.assertEqual(dB(A,B), 2.0)
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
