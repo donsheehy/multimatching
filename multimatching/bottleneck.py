571c995a94c3c9dac2428baec7708d406a4188ef
rrastog2 <rrastog2@ncsu.edu>
2022-02-10 15:55:44 -0500

    Update bottleneck.py and test_bottleneck.py to return matching

--- multimatching/bottleneck.py
+++ multimatching/bottleneck.py
@@ -1,41 +1,58 @@
 from collections import namedtuple
+
 from ortools.graph import pywrapgraph
 from pdsketch import Diagram
 
 
 def dB(A: Diagram, B: Diagram, upperbound=float('inf'), get_matching=False) -> float:
+    """
+
+    :param A: Diagram from pdsketch of first PD
+    :param B: Diagram from pdsketch of second PD
+    :param upperbound: Default float('inf'). If the user knows that the distance is
+    guaranteed to be below a certain distance then this would speed up the calculation.
+    :param get_matching: Default False. If true, returns matching between the two PDs
+    :return:
+
+    bottleneck_distance: float
+    matching: dict, returned if get_matching is set to true
+    """
     n_A = sum(A.mass.values())
     n_B = sum(B.mass.values())
     n = len(A) + 1
-    diag_A = len(A)
-    diag_B = len(A) + len(B) + 1
-    source = diag_B + 1
+    diagonal_A = len(A)
+    diagonal_B = len(A) + len(B) + 1
+    source = diagonal_B + 1
     sink = source + 1
     expected_flow = n_A + n_B
-
     edges = []
     Edge = namedtuple('Edge', ['distance', 'start', 'end', 'capacity'])
+    A_point_mass_list = list()
     for i, (a, m) in enumerate(A.mass.items()):
         # add edge from source to points in A
         edges.append(Edge(0, source, i, m))
         # add edge from each point to the diagonal
-        edges.append(Edge(a.l_inf_dist(a.diagproj()), i, diag_B, m))
+        edges.append(Edge(a.l_inf_dist(a.diagproj()), i, diagonal_B, m))
+        A_point_mass_list.append((a, m))
     # Next, add the edges from B to the sink.
+    B_point_mass_list = list()
     for j, (b, m) in enumerate(B.mass.items(), start=n):
         # add edges to from points to the sink
         edges.append(Edge(0, j, sink, m))
         # add edges from diagonal to the points
-        edges.append(Edge(b.l_inf_dist(b.diagproj()), diag_A, j, m))
+        edges.append(Edge(b.l_inf_dist(b.diagproj()), diagonal_A, j, m))
+        B_point_mass_list.append((b, m))
     # Add edges with the diagonal.
-    edges.append(Edge(0, diag_A, diag_B, min(n_A, n_B)))
-    edges.append(Edge(0, source, diag_A, n_B))
-    edges.append(Edge(0, diag_B, sink, n_A))
+    edges.append(Edge(0, diagonal_A, diagonal_B, min(n_A, n_B)))
+    edges.append(Edge(0, source, diagonal_A, n_B))
+    edges.append(Edge(0, diagonal_B, sink, n_A))
     # Only include the edges from A to B that might be matched.
     for i, (a, a_mass) in enumerate(A.mass.items()):
         for j, (b, b_mass) in enumerate(B.mass.items(), start=n):
-            if a.pp_dist(b) <= min(upperbound, max(a.l_inf_dist(a.diagproj()), b.l_inf_dist(b.diagproj()))):
+            if a.pp_dist(b) <= min(upperbound,
+                                   max(a.l_inf_dist(a.diagproj()), b.l_inf_dist(b.diagproj()))):
                 edges.append(Edge(a.dist(b), i, j, min(a_mass, b_mass)))
-    # Sort the eges by length
+    # Sort the edges by length
     edges.sort()
 
     # Binary search over the edges for the bottleneck distance.
@@ -59,16 +76,50 @@ def dB(A: Diagram, B: Diagram, upperbound=float('inf'), get_matching=False) -> f
         else:
             i, j = mid, j
 
+    '''
+    Format of TP:
+        {point: {matched_point: number_matched}}
+        
+        Example: (0,1) with mass 2 is matched with (0,1) and the diagonal
+                    {(0,1): {(0,1): 1, 'diag': 1}
+    '''
     tp = {}
+    # A_point_mass_list = list(A.mass)
     if get_matching:
         for i in range(maxflow.NumArcs()):
-            a_index = maxflow.Tail(i)
-            b_index = maxflow.Head(i)
-            if (a_index != source and b_index != sink):
-                if a_index in tp and b_index in tp[a_index]:
-                        tp[a_index][b_index] += maxflow.Flow(i)
+            tail_index = maxflow.Tail(i)
+            head_index = maxflow.Head(i)
+            if tail_index != source and head_index != sink:
+                assert head_index > n-1
+                assert tail_index < n
+
+                tail_index = index_to_point_mapper(diagonal_A,
+                                                   diagonal_B,
+                                                   tail_index,
+                                                   n,
+                                                   A_point_mass_list,
+                                                   B_point_mass_list)
+                head_index = index_to_point_mapper(diagonal_A,
+                                                   diagonal_B,
+                                                   head_index,
+                                                   n,
+                                                   A_point_mass_list,
+                                                   B_point_mass_list)
+                if tail_index in tp and head_index in tp[tail_index]:
+                    tp[tail_index][head_index] += maxflow.Flow(i)
                 else:
-                    tp[a_index] = {b_index: maxflow.Flow(i)}
+                    tp[tail_index] = {head_index: maxflow.Flow(i)}
         return bottleneck.distance, tp
     else:
         return bottleneck.distance
+
+
+def index_to_point_mapper(diag_A, diag_B, index, diagram_B_offset, A_point_mass_list,
+                          B_point_mass_list):
+    if index == diag_A or index == diag_B:
+        index = 'diag'
+    elif index < diag_A:
+        index = A_point_mass_list[0][index]
+    else:
+        index = B_point_mass_list[0][index - diagram_B_offset]
+    return index
--- multimatching/test_bottleneck.py
+++ multimatching/test_bottleneck.py
@@ -1,38 +1,80 @@
-from pdsketch import Diagram
 import unittest
+
+from pdsketch import Diagram
+
 from multimatching import dB
+
 
 class TestDBwithMult(unittest.TestCase):
     def test_no_mult(self):
         A = Diagram([(3, 6), (1, 2), (6, 18), (1, 3)])
         B = Diagram([(4, 6), (7, 21)])
+
         self.assertEqual(dB(A, B), 3.0)
 
     def test_one_point_with_mult(self):
         #  Two diagrams with multiplicity
-        A = Diagram([(0,6)],[1000000000])
-        B = Diagram([(1,6)],[1000000000])
-        self.assertEqual(dB(A,B), 1.0)
+        A = Diagram([(0, 6)], [1000000000])
+        B = Diagram([(1, 6)], [1000000000])
+
+        self.assertEqual(dB(A, B), 1.0)
 
     def test_one_point_with_different_mults(self):
-        #  Two diagrams with multiplicity
-        A = Diagram([(0,6)],[1000000001])
-        B = Diagram([(1,6)],[1000000000])
-        self.assertEqual(dB(A,B), 3.0)
+        #  Two diagrams with unequal multiplicity
+        A = Diagram([(0, 6)], [1000000001])
+        B = Diagram([(1, 6)], [1000000000])
+
+        self.assertEqual(dB(A, B), 3.0)
 
     def test_multiple_points_with_different_mults(self):
         #  Two diagrams with multiplicity with points close to the diagonal
-        A = Diagram([(0,6), (0,1)],[1000000001, 100])
-        B = Diagram([(100, 101), (1,6)],[50, 1000000000])
-        self.assertEqual(dB(A,B), 3.0)
+        A = Diagram([(0, 6), (0, 1)], [1000000001, 100])
+        B = Diagram([(100, 101), (1, 6)], [50, 1000000000])
+
+        self.assertEqual(dB(A, B), 3.0)
 
     def test_unequal_size_base_sets(self):
         #  Two diagrams with different numbers of distinct points.
-        A = Diagram([(0,6), (0,1)],[1000000001, 100])
-        B = Diagram([(1,6), (100, 101), (0,8)],[1000000000, 50, 1])
-        self.assertEqual(dB(A,B), 2.0)
+        A = Diagram([(0, 6), (0, 1)], [1000000001, 100])
+        B = Diagram([(1, 6), (100, 101), (0, 8)], [1000000000, 50, 1])
 
+        self.assertEqual(dB(A, B), 2.0)
 
+    def test_infinite_coords_with_no_mult(self):
+        A = Diagram([(0, float('inf'))])
+        B = Diagram([(2, float('inf'))])
+
+        self.assertEqual(dB(A, B), 2.0)
+
+    def test_infinite_coords_with_equal_mult(self):
+        A = Diagram([(0, float('inf'))], [500])
+        B = Diagram([(2, float('inf'))], [500])
+
+        self.assertEqual(dB(A, B), 2.0)
+
+    def test_infinite_coords_with_unequal_mult(self):
+        A = Diagram([(0, float('inf'))], [501])
+        B = Diagram([(2, float('inf'))], [500])
+
+        self.assertEqual(dB(A, B), float('inf'))
+
+    def test_finite_and_infinite_points_with_mult(self):
+        A = Diagram([(0, float('inf')), (0, 6)], [500, 2])
+        B = Diagram([(2, float('inf')), (0, 7)], [500, 2])
+
+        self.assertEqual(dB(A, B), 2.0)
+
+    def test_distance_with_self(self):
+        A = Diagram([(6, 12345)], [15])
+
+        self.assertEqual(dB(A, A), 0)
+
+    def test_mult_without_mass_list(self):
+        A = Diagram([(0, 6)], [1])
+        B = Diagram([(1, 6)])
+        bottleneck_distance, matching = dB(A, B, get_matching=True)
+        print(matching)
+        self.assertEqual(bottleneck_distance, 1.0)
 
 
 if __name__ == '__main__':
