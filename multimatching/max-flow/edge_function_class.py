f27e9a8acf6ef4a57edf6b2f3a6df7f8d0b4b3c2
pkrish22 <pkrish22@ncsu.edu>
2022-06-15 13:16:54 -0400

    changed edge function to extend defaultdict class for simpler implementation

--- multimatching/max-flow/edge_function_class.py
+++ multimatching/max-flow/edge_function_class.py
@@ -1,68 +1,41 @@
-class EdgeFunction:
+from collections import defaultdict
+
+class EdgeFunction(defaultdict):
     #initialize the edge
     def __init__(self):
-        self.values = {}
-    
-    #returns item at index
-    def __getitem__(self, index):
-        return self.values.get(index, 0)
-
-    #sets item at key to the value
-    def __setitem__(self, key, value):
-        self.values[key] = value
-
-    #deletes item with key
-    def __delitem__(self, key):
-        del self.values[key]
+        super(EdgeFunction, self).__init__(lambda:0)
 
     #incrementally adds every value in edge with value of passed object
     def __iadd__(self, other):
-        for k,v in other.items():
-            try:
-                self.values[k] += v
-            except:
-                self.values[k] = v
+        for k in other.keys():
+            self[k] += other[k]
         return self
 
     #incrementally subtracts every value in edge with value of passed object
     def __isub__(self, other):
-        for k,v in other.items():
-            try:
-                self.values[k] -= v
-            except:
-                self.values[k] = -v
+        for k in other.keys():
+            self[k] -= other[k]
         return self
 
     #defines creating an iterator
     def __iter__(self):
         return iter(self.values)
 
-    #returns the items in the list
-    def items(self):
-        return self.values.items()
-
     #returns the minimum capacity of an edge in the list
     def min_cap(self):
-        return self.values[min(self.values, key = self.values.get)]
+        return min(self.values())
+
+    #returns max capacity in the edge function
+    def max_cap(self):
+        return max(self.values())
 
     #sets all capacities to the same integer
     def set_all_cap(self, i):
-        for key in self.values:
-            self.values[key] = i
-
-    #returns length of list
-    def __len__(self):
-        return len(self.values)
+        for e in self.keys():
+            self[e] = i
 
     #subtracts other from the reverse of the edge (vu instead of uv)
     def reverseSub(self, other):
-        for k,v in other.items():
+        for k in other.keys():
             x,y = k[0], k[1]
-            try:
-                self.values[y, x] -= v
-            except:
-                self.values[y, x] = -v
-            
-    #returns max capacity in the edge function
-    def max_cap(self):
-        return(max(self.values.values()))
+            self[y, x] -= other[x, y]
